9938f1855f48e4278069ac26d09d0161bf94862f
vishal-tanwar <codageui@gmail.com>
2023-04-27 00:16:32 +0530

    done api

--- app/src/main/java/com/example/networkcall/ui/TodoViewmodel.kt
+++ app/src/main/java/com/example/networkcall/ui/TodoViewmodel.kt
@@ -6,23 +6,20 @@ import com.example.networkcall.model.Todou
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import okhttp3.internal.wait
 
 class TodoViewmodel(val repository: TodoRepository = TodoRepository()) : ViewModel() {
 
-    // resposne
-
     // call api with params
-    fun callApi(id:String)  : MutableLiveData<NetworkResult<Todou>>  {
-        var  movieResponse = MutableLiveData<NetworkResult<Todou>>()
+    fun callApi(id:String)  : MutableLiveData<NetworkResult<Todou>> {
+        var movieResponse = MutableLiveData<NetworkResult<Todou>>()
 
-        viewModelScope.launch(Dispatchers.IO){
+        viewModelScope.launch(Dispatchers.IO) {
             repository.getPopularMovies(id).collectLatest {
                 movieResponse.postValue(it)
             }
         }
         return movieResponse
+
     }
 
 }
